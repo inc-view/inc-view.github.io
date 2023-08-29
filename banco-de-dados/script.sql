e8fae029ddaf30aad5c6b1c57ba6e6848ff8fa0f
GuDantas <gustavo.em.dantas@gmail.com>
2023-08-29 13:14:15 -0300

    Modelagem atualizada com dicas do brandão

--- banco-de-dados/script.sql
+++ banco-de-dados/script.sql
@@ -32,15 +32,10 @@ nomeFuncionario VARCHAR(45) NOT NULL,
 telefoneFuncionario VARCHAR(45),
 cpfGestor VARCHAR(14),
 emailFuncionario VARCHAR(45) NOT NULL,
-fkSetor INT, FOREIGN KEY(fkSetor) REFERENCES setor(IdSetor),
+fkEmpresa INT, FOREIGN KEY(fkEmpresa) REFERENCES empresa(IdEmpresa),
 senhaGestor VARCHAR(45),
 fkGestor INT, FOREIGN KEY(fkGestor) REFERENCES funcionario(IdFuncionario)
 )AUTO_INCREMENT = 1000;
-
-CREATE TABLE chamados(
-idChamado INT PRIMARY KEY AUTO_INCREMENT,
-cartegoriaChamado VARCHAR(45) NOT NULL,
-descricaoChamado VARCHAR(250)) AUTO_INCREMENT = 1500;
 
 
 CREATE TABLE computadores(
@@ -56,12 +51,12 @@ cartegoriaSoftware VARCHAR(45))AUTO_INCREMENT = 2300;
 
 
 CREATE TABLE computadoresSoftwares(
+idComputadoresSoftwares INT NOT NULL AUTO_INCREMENT,
 FKidComputador INT, FOREIGN KEY(FKidComputador) REFERENCES computadores(IdComputador),
 FKidSoftware INT, FOREIGN KEY(FKidSoftware) REFERENCES softwares(IdSoftwares),
-PRIMARY KEY(FKidComputador, FKidSoftware),
+PRIMARY KEY(idComputadoresSoftwares, FKidComputador, FKidSoftware),
 usuario VARCHAR(45) NOT NULL,
-bloqueado VARCHAR(2),
-FKidChamado INT, FOREIGN KEY(FKidChamado) REFERENCES chamados(IdChamado));
+bloqueado VARCHAR(2))AUTO_INCREMENT = 3000;
 
 
 CREATE TABLE componentes(
@@ -70,7 +65,14 @@ tipo VARCHAR(50) NOT NULL,
 fkComputadores INT, FOREIGN KEY(fkComputadores) REFERENCES computadores(IdComputador)
 )AUTO_INCREMENT = 4000;
 
-CREATE TABLE regisitros(
+CREATE TABLE chamados(
+idChamado INT PRIMARY KEY AUTO_INCREMENT,
+cartegoriaChamado VARCHAR(45) NOT NULL,
+descricaoChamado VARCHAR(250),
+fkComputadoresSoftwares INT, FOREIGN KEY(fkComputadoresSoftwares) REFERENCES computadoresSoftwares(idComputadoresSoftwares),
+fkComponentes INT, FOREIGN KEY(fkComponentes) REFERENCES componentes(IdComponente))AUTO_INCREMENT = 1500;
+
+CREATE TABLE registros(
 IdRegistro INT PRIMARY KEY AUTO_INCREMENT,
 emUso VARCHAR(10),
 temperatura VARCHAR(5),

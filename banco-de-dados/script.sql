fa9a3bfb11458517975975516cc4fada63d1fa89
GuDantas <gustavo.em.dantas@gmail.com>
2023-09-11 14:23:48 -0300

    Script atualizado e Inserts criados

--- banco-de-dados/script.sql
+++ banco-de-dados/script.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE VISS;
-USE VISS;
 
+USE VISS;
 CREATE TABLE empresa(
 IdEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 razao VARCHAR(45) NOT NULL,
@@ -32,15 +32,10 @@ nomeFuncionario VARCHAR(45) NOT NULL,
 telefoneFuncionario VARCHAR(45),
 cpfGestor VARCHAR(14),
 emailFuncionario VARCHAR(45) NOT NULL,
-fkSetor INT, FOREIGN KEY(fkSetor) REFERENCES setor(IdSetor),
+fkEmpresa INT, FOREIGN KEY(fkEmpresa) REFERENCES empresa(IdEmpresa),
 senhaGestor VARCHAR(45),
 fkGestor INT, FOREIGN KEY(fkGestor) REFERENCES funcionario(IdFuncionario)
 )AUTO_INCREMENT = 1000;
-
-CREATE TABLE chamados(
-idChamado INT PRIMARY KEY AUTO_INCREMENT,
-cartegoriaChamado VARCHAR(45) NOT NULL,
-descricaoChamado VARCHAR(250)) AUTO_INCREMENT = 1500;
 
 
 CREATE TABLE computadores(
@@ -48,27 +43,29 @@ IdComputador INT PRIMARY KEY AUTO_INCREMENT,
 patrimonio VARCHAR(45),
 fkFuncionario INT, FOREIGN KEY(fkFuncionario) REFERENCES funcionario(IdFuncionario))AUTO_INCREMENT = 3000;
 
-
 CREATE TABLE softwares(
 IdSoftwares INT PRIMARY KEY AUTO_INCREMENT,
 nomeSoftware VARCHAR(45) NOT NULL,
+usuario VARCHAR(45) NOT NULL,
 cartegoriaSoftware VARCHAR(45))AUTO_INCREMENT = 2300;
 
 
 CREATE TABLE computadoresSoftwares(
+idComputadoresSoftwares INT NOT NULL AUTO_INCREMENT,
 FKidComputador INT, FOREIGN KEY(FKidComputador) REFERENCES computadores(IdComputador),
 FKidSoftware INT, FOREIGN KEY(FKidSoftware) REFERENCES softwares(IdSoftwares),
-PRIMARY KEY(FKidComputador, FKidSoftware),
+PRIMARY KEY(idComputadoresSoftwares, FKidComputador, FKidSoftware),
 usuario VARCHAR(45) NOT NULL,
-bloqueado VARCHAR(2),
-FKidChamado INT, FOREIGN KEY(FKidChamado) REFERENCES chamados(IdChamado));
+bloqueado VARCHAR(2))AUTO_INCREMENT = 3000;
 
 
 CREATE TABLE componentes(
 IdComponente INT PRIMARY KEY AUTO_INCREMENT,
 tipo VARCHAR(50) NOT NULL,
+modelo VARCHAR(45),
 fkComputadores INT, FOREIGN KEY(fkComputadores) REFERENCES computadores(IdComputador)
 )AUTO_INCREMENT = 4000;
+select * from componentes;
 
 
 CREATE TABLE registros(
@@ -78,6 +75,13 @@ temperatura VARCHAR(5),
 fkComponentes INT, FOREIGN KEY(fkComponentes) REFERENCES componentes(IdComponente))AUTO_INCREMENT = 5000;
 
 
-
+CREATE VIEW infoMaquina AS
+SELECT func.nomeFuncionario, computadores.patrimonio, compo.modelo,  reg.emUso, reg.Temperatura,
+CASE 
+	WHEN reg.Temperatura is null OR reg.Temperatura = '' THEN 'Componente sem aferição de temperaturo' 
+    WHEN reg.emUso is null OR reg.emUso = '' THEN 'Componente sem aferição de uso'  END as estado
+		FROM funcionario as func JOIN computadores ON IdFuncionario = fkFuncionario
+		JOIN componentes as compo ON  IdComputador = fkComputadores
+        JOIN registros as reg ON fkComponentes = IdComponente;
 
 
